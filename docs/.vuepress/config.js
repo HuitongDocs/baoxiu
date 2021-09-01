06a7ea1e67647854479475000387f84781ce38d9
Le Wang <lewang.dev@gmail.com>
2021-09-01 21:57:44 +0800

    update theme config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,6 +3,10 @@ module.exports = {
     title: '叮当报修帮助文档',
     description: '快速开始指南',
     themeConfig: {
+        editLinks: true,
+        editLinkText: '在 GitHub 上编辑此页面',
+        repo: 'https://github.com/HuitongDocs/baoxiu',
+        lastUpdated: '上次更新',
         sidebar: [
             './',
             './guide/Repair',
